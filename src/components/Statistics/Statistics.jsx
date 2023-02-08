f1d1fc4a7bd177f5cdccee37259b32aae0ff8b29
Tetiana Novitska <novittana2410@gmail.com>
2023-02-09 00:15:32 +0200

    init

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css'
+import {getRandomHexColor} from '../../utils/getRandomHexColor'
+
+
 
 export function Statistics({ title = '', stats }) {
+  
   return (
     <section className={css.statistics}>
-      <h2 className={title ? css.title : css.title_none}>{title.toUpperCase()}</h2>
+      <h2 className={title ? css.title : css.title_none}>
+        {title.toUpperCase()}
+
+      </h2>
 
       <ul className={css.stat_list}>
         {stats.map(({ id, label, percentage }) => (
-          <li key={id} className={css.item}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}%</span>
+          <li
+            key={id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor(),}}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
--- src/utils/GetRandomHexColor.js
+++ src/utils/GetRandomHexColor.js
@@ -1,5 +1,5 @@
-// export function GetRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+export function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
