93f3564a518486b2ddeeb10ea8a6c0786d371615
Tetiana Novitska <novittana2410@gmail.com>
2023-02-09 09:06:46 +0200

    init

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css'
-import {getRandomHexColor} from '../../utils/getRandomHexColor'
+import {getRandomHexColor} from 'utils/getRandomHexColor'
 
 
 
